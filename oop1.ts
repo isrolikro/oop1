c76f0dd4661e43c549dfc69be3f6e2266061607a
isrolik <isralerosn@gmail.com>
2023-08-27 11:42:08 +0300

    2

--- oop1.ts
+++ oop1.ts
@@ -1,30 +1,77 @@
 // 1:
-class Rectangle  {
-    height:number
-    width:number
-    
-    constructor(height:number, width:number) {
-        this.height = height
-        this.width = width
-    }
-    area():number{
-        return this.height * this.width
-    }
+class Rectangle {
+  height: number;
+  width: number;
+
+  constructor(height: number, width: number) {
+    this.height = height;
+    this.width = width;
+  }
+  area(): number {
+    return this.height * this.width;
+  }
 }
 // 2:
 class Square extends Rectangle {
-    a: number
+  a: number;
 
-    constructor(a:number){
-    super(a,a)
-      this.a = a
-    }
+  constructor(a: number) {
+    super(a, a);
+  }
+  area(): number {
+    return this.height * this.width;
+  }
 }
 
-const 
-// git init
-// git add .
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/isrolikro/oop1.git
-// git push -u origin main
+// 3:
+class Shape {
+  info(): string {
+    return "This is a Shape";
+  }
+}
+
+class Rectangle2 extends Shape {
+  info(): string {
+    return "This is a Recktangle";
+  }
+}
+class ColoredRectangle extends Rectangle {
+  color: string;
+
+  constructor(height: number, width: number, color: string) {
+    super(width, height);
+    this.color;
+  }
+
+  info(): string {
+    return `This is a rectangle of color ${this.color}`;
+  }
+}
+
+// 5:
+class Shape2 {
+  draw(): void {
+    console.log("drawing a shape");
+  }
+}
+class Triangle extends Shape2 {
+  draw(): void {
+    console.log("this is Triangle");
+  }
+}
+class Circle extends Shape2 {
+  draw(): void {
+    console.log("this is Circle");
+  }
+}
+class Square1 extends Shape2 {
+  draw(): void {
+    console.log("this is Triangle Square");
+  }
+}
+
+function renderShapes(shapes: Shape2[]): void {
+  for (const Shape2 of shapes) {
+    Shape2.draw();
+  }
+}
